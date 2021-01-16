052ac808a05de484bfc82fe1da59074c718142bf
Aline Venceslau <alineepecris79@gmail.com>
2021-01-16 20:55:40 -0300

    cadastrando apenas usuarios que colocam as informações corretas no formulario

--- php/dados.php
+++ php/dados.php
@@ -1,5 +1,33 @@
 <?php
-    $fp = fopen('cadastro.csv', 'w'); 
-    fwrite($fp, $_POST['nomeUsuario'] . ";" . $_POST['email'] . ";". $_POST['senha'] . ";" . $_POST['confirma'] .";\n");
-    fclose($fp);
+if($_POST){
+	$nome = $_POST['nomeUsuario'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+	$confirma = $_POST['confirma'];
+
+	if($nome === ""){
+		echo  "<script>alert('Campo nome de usuario não foi preenchido, refaça seu cadastro');</script>";
+	}
+	else if($email === ""){
+		echo  "<script>alert('Campo email não foi preenchido, refaça seu cadastro');</script>";
+	}else{
+
+		if($senha === ""){
+			echo  "<script>alert('Campo senha não foi preenchido, refaça seu cadastro');</script>";
+		}
+		else if($confirma === ""){
+			echo  "<script>alert('Campo confirmar senha não foi preenchido, refaça seu cadastro');</script>";
+		}
+		else if($senha === $confirma){
+			
+			echo  "<script>alert('Cadastro criado com Sucesso!');</script>";
+			$fp = fopen('cadastro.csv', 'a'); 
+			fwrite($fp, $_POST['nomeUsuario'] . ";" . $_POST['email'] . ";". $_POST['senha'] . ";" . $_POST['confirma'] .";\n");
+			fclose($fp);
+		}
+		else{
+			echo  "<script>alert('Senhas diferentes, refaça seu cadastro');</script>";	
+		}
+	}
+}
 ?>
