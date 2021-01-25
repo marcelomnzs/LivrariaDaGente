9b83611ef9eb7bb1f81fe5bdbe206c5a4db9684b
pecrisandrade <pecrisandrade@gmail.com>
2021-01-25 12:26:16 -0300

    lista dos livros cadastrados pelo usuario

--- php/livroscadastrados.php
+++ php/livroscadastrados.php
@@ -6,38 +6,39 @@ for ($i=0; $i < sizeof($livro); $i++) {
 }
 
 
- ?>
- <!DOCTYPE html>
- <html>
- <head>
- 	<title>Meu Acervo</title>
- </head>
- <body>
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meu Acervo</title>
+</head>
+<body>
 
- <h1>Meu Acervo</h1>
- <h2>Livros Adicionados</h2>
+	<h1>Meu Acervo</h1>
+	<h2>Livros Adicionados</h2>
 
- <table>
- 	<tr>
- 		<th>Nome Do Livro</th>
-        <th>Autor</th>
-        <th>Edição</th>
-        <th>Gênero</th>
-        <th>Estado</th> 	
- 	</tr>
-	<?php foreach ($livro as $linha => $liv): ?>
-		    <tr>
-                <td><?= $liv[0] ?></td>
-                <td><?= $liv[1] ?></td>
-                <td><?= $liv[2] ?></td>
-                <td><?= $liv[3] ?></td>
-                <td><?= $liv[4] ?></td>
-                <td><a href="deletlivros.php?linha=<?= $linha ?>">Deletar Livro</a></td>
-            </tr>
-        <?php endforeach ?>
-</table>
+	<table>
+		<tr>
+			<th>Nome Do Livro</th>
+			<th>Autor</th>
+			<th>Edição</th>
+			<th>Gênero</th>
+			<th>Estado</th> 	
+		</tr>
+		<?php foreach ($livro as $linha => $liv): ?>
+			<tr>		   	
+				<td><?= $liv[0] ?></td>
+				<td><?= $liv[1] ?></td>
+				<td><?= $liv[2] ?></td>
+				<td><?= $liv[3] ?></td>
+				<td><?= $liv[4] ?></td>
+				<td><a href="deletlivros.php?linha=<?= $linha ?>">Deletar Livro</a></td>
+			</tr>
+			
+		<?php endforeach ?>
+	</table>
 
-<a href="addlivro.php">Adicionar Livro</a>
+	<a href="addlivro.php">Adicionar Livro</a>
 
- </body>
- </html>
+</body>
+</html>
