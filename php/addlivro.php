d3e3b98166cc954d1727225f96288edb23e00a6f
pecrisandrade <pecrisandrade@gmail.com>
2021-01-25 12:23:51 -0300

    arquivo para adicionar livro ao banco

--- php/addlivro.php
+++ php/addlivro.php
@@ -10,28 +10,27 @@
 <legend>Adicionar Livro</legend>		
 <input type="text" name="nome" placeholder="Nome" maxlength="45">
 <input type="text" name="autor" placeholder="Autor" maxlength="25">
-<input type="text" name="edicao" placeholder="Edição" maxlength="10">
+<input type="number" min="1" max="999" name="edicao" placeholder="Edição" >
 <select name="genero">
 	<option value="genero" selected>Gênero</option>
-	<option value="acao">Ação</option>
-	<option value="ficcao">Ficção</option>
-	<option value="aventura">Aventura</option>
-	<option value="romance">Romance</option>
-	<option value="biografia">Biografia</option>
-	<option value="hq">HQ</option>
-	<option value="manga">Mangá</option>
-	<option value="didaticos">Didáticos</option>
-	<option value="literatura">Literatura Brasileira</option>
-	<option value="poesia">Poesia</option>
-	<option value="literatura">Literatura Estrangeira</option>
-	<option value="terror">Terror</option>
-	<option value="outros">Outros</option>
+	<option value="Ação">Ação</option>
+	<option value="Ficção">Ficção</option>
+	<option value="Aventura">Aventura</option>
+	<option value="Romance">Romance</option>
+	<option value="Biografia">Biografia</option>
+	<option value="HQ/Mangá">HQ/Mangá</option>
+	<option value="Didáticos">Didáticos</option>
+	<option value="Literatura Brasileira">Literatura Brasileira</option>
+	<option value="Poesia">Poesia</option>
+	<option value="Literatura Estrangeira">Literatura Estrangeira</option>
+	<option value="Terror">Terror</option>
+	<option value="Outros">Outros</option>
 </select>
 <select name="estado">
 	<option value="estado">Estado</option>
-	<option value="novo">Novo</option>
-	<option value="usado">Usado</option>
-	<option value="desgastado">Desgastado</option>
+	<option value="Novo">Novo</option>
+	<option value="Conservado">Usado</option>
+	<option value="Usado">Desgastado</option>
 	</select>
 <input type="submit" value="Adicionar">
 </fieldset>
