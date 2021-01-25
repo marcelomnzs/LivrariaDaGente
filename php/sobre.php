3a7a45cb0530e1446e47b6e7a69739d430bb3ad2
alinevenceslau <alineepecris79@gmail.com>
2021-01-25 15:15:04 -0300

    criando arquivo para o sobre

---
+++ php/sobre.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" type="image/png" sizes="32x32" href="../img/favicon-96x96.png">
+	<link rel="stylesheet" type="text/css" href="../css/sobre.css">
+	<title>Sobre</title>
+</head>
+	<body>
+	 <div id="navbar">
+            <a href="../index.php"><img src="../img/logo.png" alt="Logo Livraria da Gente" id="logo"></a>
+            
+        </div>
+	</body>
+</html>
