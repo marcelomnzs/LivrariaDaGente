d32aa21df117105e6a088c87c6cfb13b2ad567b3
alinevenceslau <alineepecris79@gmail.com>
2021-01-25 15:15:26 -0300

    criando arquivo para o acervo

---
+++ php/acervo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" type="image/png" sizes="32x32" href="../img/favicon-96x96.png">
+	<link rel="stylesheet" type="text/css" href="../css/acervo.css">
+	<title>Acervo</title>
+</head>
+	<body>
+	 	<header>
+        <div id="navbar">
+            <a href="../index.php"><img src="../img/logo.png" alt="Logo Livraria da Gente" id="logo"></a>
+        </div>
+    </header>
+	</body>
+</html>
