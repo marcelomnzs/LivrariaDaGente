fd357f7a234c137a5a50bb782ee9a44914ca9d39
alinevenceslau <alineepecris79@gmail.com>
2021-02-02 03:12:45 -0300

    fazendo verificação do login

--- php/validlogin.php
+++ php/validlogin.php
@@ -1,10 +1,7 @@
 <!-- tentando validar o login -->
 
 <?php
-$cadastrados = file('cadastro.csv');
-for ($i = 0; $i < sizeof($cadastrados); $i++){
-	$cadastrados[$i] = explode(';',$cadastrados[$i]);
-}
+include '../conect.php';
 
 if($_POST){
 	$encontrado = 0;
@@ -13,33 +10,37 @@ if($_POST){
 
 	if($login_email === ""){
 		echo  "<script>alert('Campo de email não foi preenchido, tente novamente!');</script>";
-					header("Refresh: 0; url = /php/login.php");
+		header("Refresh: 0; url = /php/login.php");
 
 	}
 	else if($login_senha === ""){
 		echo  "<script>alert('Campo senha não foi preenchido, tente novamente!');</script>";
 		header("Refresh: 0; url = /php/login.php");
 	}else{
-		for($i = 0; $i < sizeof($cadastrados); $i++){	
-			if($login_email === $cadastrados[$i][1]){
-				if ($login_senha === $cadastrados[$i][2]){
-					echo "<script>alert('acesso liberado');</script>";
-					header("Refresh: 0; url = perfil.php");
 
+			$livraria = $con -> prepare("SELECT email FROM usuario WHERE email=? AND senha=?;"); 
+			$livraria -> bindParam(1,$login_email);
+			$livraria -> bindParam(2,$login_senha);
+			$livraria -> execute();
+			$f = $livraria -> fetch();
+			
+			if($f["email"] != null){
+				header("Refresh: 0; url = perfil.php");
 
-					$encontrado = 1;
-				}else{
-					echo "<script>alert('senha incorreta');</script>";
+				$encontrado = 1;
+			}else{
+					echo "<script>alert('Email ou senha incorreto!');</script>";
+
 					header("Refresh: 0; url = login.php");
 					exit;
 
 				}
 			}
-		}
 		if($encontrado == 0){
 			echo "<script>alert('Email não cadastrado');</script>";
 			header("Refresh: 0; url = login.php");
 		}
 	}
-}
+	
+
 ?>
