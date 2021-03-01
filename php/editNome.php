9c9ba8f17d0f5d721d52f0d38d77b0c6560931a6
Heitor <heitorgomes500@outlook.com>
2021-03-01 09:56:06 -0300

    Tentando editar Nome

--- php/editNome.php
+++ php/editNome.php
@@ -4,29 +4,26 @@ require 'init.php';
 $atual = $_POST['nome'];
 $novo = $_POST['novo'];
 
+$user= $_SESSION['usuario'];
 
-$id = $_GET['id'];
+$id= $_GET['id'];
 
 
 
-if ($atual == $novo ) {
-	echo "<script>alert('Digite um nome diferente do atual');</script>";
-} else {
-   
    require '../conect.php';
 
-  $stmt = $con->prepare("SELECT nome FROM usuario WHERE id =?");
+  $stmt = $con->prepare("SELECT * FROM usuario WHERE id =?");
   $stmt->execute([$id]);
 
 
-if($stmt->rowCount() > 0 && $stmt->fetch()['nome'] == $id) {
+if($stmt->rowCount() > 0 && $stmt->fetch()['nome'] == $user) {
    $stmt = $con->prepare("
-       UPDATE nome FROM usuario SET nome=? WHERE  id=?
+       UPDATE usuario SET nome= ? WHERE  id=?
        ");
-   $stmt->execute([$nome,$id]);
+   $stmt->execute([$user,$id]);
    }
 
-}
+
 
 header('location:perfil.php');
 
