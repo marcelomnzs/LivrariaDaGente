86056b2e3e996b995fd8fd68fe3b723b71dd61b3
Marcelo Antonio <marceloa.mnzs@gmail.com>
2021-03-07 21:15:53 -0300

    Alterando a estilização dos formulários de adição de livro e de edição de livro

--- php/addlivro.php
+++ php/addlivro.php
@@ -1,73 +1,88 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Adicionar Livro</title>
 	<link rel="icon" type="image/png" sizes="32x32" href="../img/favicon-96x96.png">
 	<link rel="stylesheet" href="../css/addlivro.css">
-	<?= include "linksbootstrap.php";?>
+	<?= require "linksbootstrap.php"; ?>
 
 </head>
+
 <body>
-	<form class="login-wrap" action="validlivro.php" method="POST">
-	  <div class="form-row">
-	    <div class="col-md-4 mb-3">
-	      <label for="validationDefault01">Nome</label>
-	      <input type="text" class="form-control" id="validationDefault01" name="nome" placeholder="Título do Livro" required>
-	    </div>
-	    <div class="col-md-4 mb-3">
-	      <label for="validationDefault02">Autor</label>
-	      <input type="text" class="form-control" id="validationDefault02" name="autor" placeholder="Autor" required>
-	    </div>
-	    <div class="col-md-4 mb-3">
-	      <label for="validationDefault02">SubTitulo</label>
-	      <input type="text" class="form-control" id="validationDefault02" name="subtitulo" placeholder="SubTitulo" maxlength="100" required>
-	    </div>
-	    <div class="col-md-6 mb-3">
-	      <label for="validationDefault02">ISBN</label>
-	      <input type="text" class="form-control" id="validationDefault02" name="isbn" placeholder="ISBN ou ASIN" required>
-	    </div>
-	    <div class="col-md-6 mb-3">
-	      <label for="validationDefaultUsername">Edição</label>
-	      <div class="input-group">
-	        <input type="number" class="form-control" min="1" max="999" name="edicao" placeholder="Edição do Livro" required>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="form-row">
-	    <div class="col-md-6 mb-3">
-	      <label for="validationDefault03">Gênero</label>
-		  <select name="genero" class="form-control" required>
-				<option value="genero" selected disabled>Escolha uma opção...</option>
-				<option value="Ação">Ação</option>
-				<option value="Ficção">Ficção</option>
-				<option value="Aventura">Aventura</option>
-				<option value="Romance">Romance</option>
-				<option value="Biografia">Biografia</option>
-				<option value="HQ/Mangá">HQ/Mangá</option>
-				<option value="Didáticos">Didáticos</option>
-				<option value="Literatura Brasileira">Literatura Brasileira</option>
-				<option value="Poesia">Poesia</option>
-				<option value="Literatura Estrangeira">Literatura Estrangeira</option>
-				<option value="Terror">Terror</option>
-				<option value="Outros">Outros</option>
-			</select>
-	    </div>
-	    <div class="col-md-6 mb-3">
-	      <label for="validationDefault04">Estado</label>
-		  <select name="estado" class="form-control" required >
-				<option value="estado" disabled selected>Estado</option>
-				<option value="Novo">Novo</option>
-				<option value="Conservado">Usado</option>
-				<option value="Usado">Desgastado</option>
-			</select>
-	    </div>
-	  </div>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-7">
+				<h2 class="h3 my-5 page-title text-center">Adicionar Livro</h2>
+				<div class="img-fluid col-md-2 float-right imagemLivro">
+					<img src="../img/adicionandoLivro.png" alt="Ilustração de personagem alterando as configurações do perfil">
+				</div>
+				<form action="validlivro.php" method="POST">
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label>Nome:</label>
+							<input type="text" name="nome" class="form-control" placeholder="Jogos Vorazes" required>
+						</div>
+						<div class="form-group col-md-6">
+							<label>Subtítulo:</label>
+							<input type="text" name="subtitulo" class="form-control" placeholder="Em chamas" required>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-12">
+							<label>Autor:</label>
+							<input name="autor" class="form-control" id="inputPassword5" placeholder="Autor da Obra" required>
+						</div>
+					</div>
 
-	  <button class="btn btn-primary" type="submit" value="Adicionar">Adicionar Livro</button>
-</form>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label>ISBN:</label>
+							<input name="isbn" class="form-control" placeholder="Apenas números">
+						</div>
+						<div class="form-group col-md-6">
+							<label>Edição:</label>
+							<input name="edicao" type="number" min="1" max="999" class="form-control" placeholder="Edição do Livro" required>
+						</div>
+					</div>
 
-<!-- Espaço destinado as fotos dos livros -->
-<div class="cards">
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label>Gênero:</label>
+							<select name="genero" class="form-control" required>
+								<option value="genero" selected disabled>Escolha uma opção...</option>
+								<option value="Ação">Ação</option>
+								<option value="Ficção">Ficção</option>
+								<option value="Aventura">Aventura</option>
+								<option value="Romance">Romance</option>
+								<option value="Biografia">Biografia</option>
+								<option value="HQ/Mangá">HQ/Mangá</option>
+								<option value="Didáticos">Didáticos</option>
+								<option value="Literatura Brasileira">Literatura Brasileira</option>
+								<option value="Poesia">Poesia</option>
+								<option value="Literatura Estrangeira">Literatura Estrangeira</option>
+								<option value="Terror">Terror</option>
+								<option value="Outros">Outros</option>
+							</select>
+						</div>
+
+						<div class="form-group col-md-6">
+							<label>Estado:</label>
+							<select name="estado" class="form-control" required>
+								<option value="estado" disabled selected>Escolha uma opção...</option>
+								<option value="Novo">Novo</option>
+								<option value="Conservado">Usado</option>
+								<option value="Usado">Desgastado</option>
+							</select>
+						</div>
+					</div>
+					<button type="submit" class="btn btn-outline-primary col-md-5 ml-1 my-2">Adicionar Livro</button>
+				</form>
+		</div>
+	</div>
+
+	<!-- Espaço destinado as fotos dos livros -->
+	<!-- <div class="cards">
 	<div class="card" style="width: 18rem;">
 	  <img class="card-img-top" src=".." alt="Card image cap">
 	  <div class="card-body">
@@ -90,7 +105,8 @@
 	    <h5 class="card-title">Verso</h5>
 	  </div>
 	</div>
-</div>
+</div> -->
 
 </body>
+
 </html>
--- php/updateform.php
+++ php/updateform.php
@@ -1,10 +1,10 @@
 <?php require 'init.php'; ?>
 
-<?php if (isset($_SESSION['usuario'])): ?>
+<?php if (isset($_SESSION['usuario'])) : ?>
 
-<?php $id_livro = $_GET['id']; 
+    <?php $id_livro = $_GET['id'];
 
-    include '../conect.php';
+    include './conect.php';
 
     $usuario = $_SESSION['usuario'];
 
@@ -12,64 +12,95 @@
     $stmt = $con->prepare("SELECT * FROM livro WHERE id = ? ;");
     $stmt->execute([$id_livro]);
     $livros = $stmt->fetch();
-?>
+    ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edição de livro</title>
-    <link rel="stylesheet" href="../css/updateform.css">
-    <?= include "linksbootstrap.php"?>
-</head>
-<body>
-    <form action="editarlivro.php?id=<?=$id_livro?>" class="login-wrap" method="POST">
-        <div class="form-row">
-            <div class="col-md-4 mb-3">
-                <label>Nome do Livro:</label>
-                <input type="text" name="titulo" class="form-control" placeholder="Nome do Livro" value="<?= $livros['titulo'] ?>">
-            </div>
+    <!DOCTYPE html>
+    <html lang="pt-br">
 
-            <div class="col-md-4 mb-3">
-                <label>Autor:</label>
-                <input type="text" name="autor" class="form-control" placeholder="Autor" value="<?= $livros['autor'] ?>">
-            </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Edição de livro</title>
+        <link rel="stylesheet" href="../css/updateform.css">
+        <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon-96x96.png">
+        <?= include "linksbootstrap.php" ?>
+    </head>
 
-            <div class="col-md-4 mb-3">
-                <label>Subtitulo:</label>
-                <input type="text" name="subtitulo" class="form-control" placeholder="Subtitulo" value="<?= $livros['subtitulo'] ?>">
-            </div>
-            
-            <div class="col-md-6 mb-3">
-                <label>Isbn:</label>
-                <input type="text" name="isbn" class="form-control" placeholder="Isbn" value="<?= $livros['isbn'] ?>">
-            </div>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-7">
+                    <h2 class="h3 my-5 page-title text-center">Edição de Informações</h2>
+                    <div class="img-fluid col-md-3 float-right ">
+                        <img src="../img/editarLivro.png" alt="Ilustração de personagem alterando as configurações do perfil">
+                    </div>
+                    <form action="editarlivro.php?id=<?= $id_livro ?>" method="POST">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>Nome:</label>
+                                <input type="text" name="titulo" class="form-control" placeholder="Jogos Vorazes" value="<?= $livros['titulo'] ?>" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Subtítulo:</label>
+                                <input type="text" name="subtitulo" class="form-control" placeholder="Em chamas" value="<?= $livros['subtitulo'] ?>" required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <label>Autor:</label>
+                                <input name="autor" class="form-control" placeholder="Autor da Obra" value="<?= $livros['autor'] ?>" required>
+                            </div>
+                        </div>
 
-            <div class="col-md-6 mb-3">
-                <label>Edicao:</label>
-                <input type="text" name="edicao" class="form-control" placeholder="Edição" value="<?= $livros['edicao'] ?>">
-            </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>ISBN:</label>
+                                <input name="isbn" class="form-control" placeholder="Apenas números" value="<?= $livros['isbn'] ?>">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Edição:</label>
+                                <input name="edicao" type="number" min="1" max="999" class="form-control" placeholder="Edição do Livro" value="<?= $livros['edicao'] ?>" required>
+                            </div>
+                        </div>
 
-            <div class="col-md-6 mb-3">
-                <label>Gênero:</label>
-                <input type="text" name="genero" class="form-control" placeholder="Gênero" value="<?= $livros['genero'] ?>">
-            </div>
-            
-            <div class="col-md-6 mb-3">
-                <label>Estado:</label>
-                <input type="text" name="estado" class="form-control" placeholder="Estado" value="<?= $livros['estado'] ?>">
-            </div>
-            <!-- <input type="hidden" name="id"> -->
-            <button class="btn btn-primary float-end " role="button" type="Submit">Enviar</button>
-        </div>
-    </form>
-</body>
-</html>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>Gênero:</label>
+                                <select name="genero" class="form-control" required>
+                                    <option value="Ação" <?= ($livros['genero'] == 'Ação') ? 'selected' : '' ?>>Ação</option>
+                                    <option value="Ficção" <?= ($livros['genero'] == 'Ficção') ? 'selected' : '' ?>>Ficção</option>
+                                    <option value="Aventura" <?= ($livros['genero'] == 'Aventura') ? 'selected' : '' ?>>Aventura</option>
+                                    <option value="Romance" <?= ($livros['genero'] == 'Romance') ? 'selected' : '' ?>>Romance</option>
+                                    <option value="Biografia" <?= ($livros['genero'] == 'Biografia') ? 'selected' : '' ?>>Biografia</option>
+                                    <option value="HQ/Mangá" <?= ($livros['genero'] == 'HQ/Mangá') ? 'selected' : '' ?>>HQ/Mangá</option>
+                                    <option value="Didáticos" <?= ($livros['genero'] == 'Didáticos') ? 'selected' : '' ?>>Didáticos</option>
+                                    <option value="Literatura Brasileira" <?= ($livros['genero'] == 'Literatura Brasileira') ? 'selected' : '' ?>>Literatura Brasileira</option>
+                                    <option value="Poesia" <?= ($livros['genero'] == 'Poesia') ? 'selected' : '' ?>>Poesia</option>
+                                    <option value="Literatura Estrangeira" <?= ($livros['genero'] == 'Literatura Estrangeira') ? 'selected' : '' ?>>Literatura Estrangeira</option>
+                                    <option value="Terror" <?= ($livros['genero'] == 'Terror') ? 'selected' : '' ?>>Terror</option>
+                                    <option value="Outros" <?= ($livros['genero'] == 'Outros') ? 'selected' : '' ?>>Outros</option>
+                                </select>
+                            </div>
 
-<?php else: ?>
+                            <div class="form-group col-md-6">
+                                <label>Estado:</label>
+                                <select name="estado" class="form-control" required>
+                                    <option value="Novo" <?= ($livros['estado'] == 'Novo') ? 'selected' : '' ?>>Novo</option>
+                                    <option value="Conservado" <?= ($livros['estado'] == 'Conservado') ? 'selected' : '' ?>>Usado</option>
+                                    <option value="Usado" <?= ($livros['estado'] == 'Usado') ? 'selected' : '' ?>>Desgastado</option>
+                                </select>
+                            </div>
+                            <input type="hidden" name="id">
+                        </div>
+                        <button type="submit" class="btn btn-outline-primary col-md-5 ml-1 my-2">Salvar Alterações</button>
+                    </form>
+    </body>
 
-<?php header("location:login.php"); ?> 
+    </html>
+
+<?php else : ?>
+
+    <?php header("location:login.php"); ?>
 
 <?php endif ?>
